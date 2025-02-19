53e464d249cb296851d540ada1e9c0efe80757a8
Gerard Harkema <ga.harkema@avans.nl>
2025-02-19 20:13:36 +0100

    20250219

--- esp32-s3-matrix/font/src/main.cpp
+++ esp32-s3-matrix/font/src/main.cpp
@@ -2,7 +2,6 @@
 #include "WS_Flow.h"
 
 // English: Please note that the brightness of the lamp bead should not be too high, which can easily cause the temperature of the board to rise rapidly, thus damaging the board !!!
-// Chinese: 请注意，灯珠亮度不要太高，容易导致板子温度急速上升，从而损坏板子!!! 
 char Text[] ="Waveshare ESP32-S3-Matrix Text Testing!";
 void setup()
 {
